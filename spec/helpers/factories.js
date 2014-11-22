541caf7b085bccb542ab30dbffcf7f66086b4871
dezoxel <dezoxel@gmail.com>
2014-11-22 13:08:44 +0200

    [Spec] "db" and "connection" factory are linked more logically

--- spec/helpers/factories.js
+++ spec/helpers/factories.js
@@ -5,6 +5,8 @@ function Connection() {}
 
 factory.define('connection', Connection, {
 
+  state: 'connected',
+
   connect: function() {
     return function(callback) {
       callback();
@@ -13,7 +15,20 @@ factory.define('connection', Connection, {
 
   end: function() {
     return function() {};
+  },
+
+  getConnectionError: function() {
+    return function() {
+      return new Error('Unable to connect');
+    };
+  },
+
+  isConnected: function() {
+    return function() {
+      return this.state === 'connected';
+    }.bind(this);
   }
+
 });
 
 factory.define('db', DB, {
@@ -21,19 +36,22 @@ factory.define('db', DB, {
   state: 'connected',
 
   connect: function() {
+    var self = this;
     // we return function since factory-girl computes the object property in order
     // to get value, but we need method instead of value
     return function() {
 
       return new Promise(function(resolve, reject) {
-        if (this.state === 'connected') {
+        var connection = factory.buildSync('connection', {state: self.state});
+
+        if (connection.isConnected()) {
           resolve();
         } else {
-          reject(new Error('Unable to connect'));
+          reject(connection.getConnectionError());
         }
-      }.bind(this));
+      });
 
-    }.bind(this);
+    };
   },
 
   getConnection: function() {
