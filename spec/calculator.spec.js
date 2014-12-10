44f468d864098f85bbf9571aa93f9c64921486f1
dezoxel <dezoxel@gmail.com>
2014-12-10 09:46:22 +0200

    Added ability to ignore nonexistent weekdays from the input

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -7,15 +7,29 @@ describe('Calculator', function() {
     calculator = new Calculator();
   });
 
-  it('calculates total for the specified preset name and target', function() {
-    var target = {
-      full: {
-        mon: 5,
-        tue: 6
-      }
-    };
+  describe('when calc total for the specified preset name and target', function() {
 
-    expect(calculator.calcTotalFor('full', target)).to.eql(11);
+    it('summarizes values by week days', function() {
+      var target = {
+        full: {
+          mon: 5,
+          tue: 6
+        }
+      };
+
+      expect(calculator.calcTotalFor('full', target)).to.eql(11);
+    });
+
+    it('ignores nonexistent weekdays from the input', function() {
+      var target = {
+        half: {
+          aaa: 5,
+          bbb: 6
+        }
+      };
+
+      expect(calculator.calcTotalFor('half', target)).to.eql(0);
+    });
   });
 
   it('calculates totals hash for each target', function() {
