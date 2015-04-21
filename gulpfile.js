e687d148aecb967f805cee0d6de706ccd0031c7c
dezoxel <dezoxel@gmail.com>
2015-04-21 21:23:43 +0300

    [Test] Renamed gulp tasks "coffee" to "compile_coffee"

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,22 @@ gulp.task('clean', function (cb) {
   del(['dist'], cb);
 });
 
-gulp.task('test', ['coffee'], function () {
+gulp.task('test', ['compile_coffee'], function () {
   require('./spec/helpers/config');
 
   return gulp.src('./dist/spec/**/*.spec.js', {read: false})
     .pipe(mocha({reporter: 'spec'}));
 });
 
-gulp.task('coffee', ['clean', 'coffee:lib', 'coffee:spec']);
+gulp.task('compile_coffee', ['clean', 'compile_coffee:lib', 'compile_coffee:spec']);
 
-gulp.task('coffee:lib', ['clean'], function() {
+gulp.task('compile_coffee:lib', ['clean'], function() {
   return gulp.src('./lib/**/*.coffee')
     .pipe(coffee())
     .pipe(gulp.dest('./dist/lib/'));
 });
 
-gulp.task('coffee:spec', ['clean'], function() {
+gulp.task('compile_coffee:spec', ['clean'], function() {
   return gulp.src('./spec/**/*.coffee')
     .pipe(coffee())
     .pipe(gulp.dest('./dist/spec/'));
