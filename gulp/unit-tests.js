aec25d8ca6279e8b351896d75da8ca832472e8f1
dezoxel <dezoxel@gmail.com>
2015-08-01 00:39:11 +0300

    [Test] Hardcoded paths replaced with path.join()

--- gulp/unit-tests.js
+++ gulp/unit-tests.js
@@ -27,8 +27,8 @@ gulp.task('test:auto', ['watch'], function(done) {
 
 gulp.task('codeclimate', function() {
   var token = '19deb5fc3438551272f5d3352c0ec3f85175e75f91657ae1f71c02038c5f0ec6';
-  var exec = './node_modules/.bin/codeclimate-test-reporter';
-  var lcovPath = './coverage/lcov.info';
+  var exec = path.join(__dirname, '../node_modules/.bin/codeclimate-test-reporter');
+  var lcovPath = path.join(__dirname, '../coverage/lcov.info');
   var command = 'CODECLIMATE_REPO_TOKEN=' + token + ' ' + exec + ' < ' + lcovPath;
   console.log('Codeclimate: Uploading... ' + command);
 
