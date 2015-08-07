6b625286284f8af05304a841e76a89aac5c4b047
dezoxel <dezoxel@gmail.com>
2015-08-07 19:07:21 +0300

    [Backend] Added simulation of company

--- src/app/entities/backend.js
+++ src/app/entities/backend.js
@@ -3,15 +3,19 @@
 
   angular
     .module('sfba.entities')
-    .provider('backend', function(orders) {
+    .provider('backend', function(orders, cogniance) {
 
       return {
         $get: function($q) {
           return {
-            get: function() {
+            get: function(url) {
               return $q(function(resolve, reject) {
                 if (true) {
-                  resolve(orders);
+                  if (url === '/company/1') {
+                    resolve(cogniance);
+                  } else if (url.indexOf('/company/1/week') !== -1) {
+                    resolve(orders);
+                  }
                 } else {
                   reject();
                 }
---
+++ src/app/entities/backend/cogniance.js
@@ -0,0 +1,10 @@
+(function(angular) {
+  'use strict';
+
+  angular
+    .module('sfba.entities')
+    .constant('cogniance', {
+      id: 1,
+      title: 'Cogniance'
+    });
+})(angular);
