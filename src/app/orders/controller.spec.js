38a3a3e34cb264515c1853c84f55fff6d4aa6b1b
dezoxel <dezoxel@gmail.com>
2015-08-07 18:44:21 +0300

    [Test] OrdersController specs refactored

--- src/app/orders/controller.spec.js
+++ src/app/orders/controller.spec.js
@@ -7,59 +7,89 @@ describe('OrdersController', function () {
 
   beforeEach(module('sfba.orders'));
 
-  var vm, Week, $rootScope, $q, $log, Order;
-
-  beforeEach(inject(function ($controller, _$rootScope_, _$q_, moment, _Week_, _Order_) {
+  var $rootScope, $q, Week, Order, Client;
+  beforeEach('reassign injected services', inject(function (_$rootScope_, _$q_, _Week_, _Order_, _Client_) {
     $rootScope = _$rootScope_;
     $q = _$q_;
     Week = _Week_;
     Order = _Order_;
+    Client = _Client_;
+  }));
 
-    sinon.stub(Order, 'findWhere').returns($q(function(resolve) {
-      resolve([1,2,3]);
-    }));
-
+  var $log;
+  beforeEach('stub $log', function() {
     $log = {error: sinon.stub()};
+  });
 
+  var vm;
+  beforeEach('create controller', inject(function($controller, moment) {
     vm = $controller('OrdersController', {Week: Week, moment: moment, $log: $log});
   }));
 
-  afterEach(function() {
-    Order.findWhere.restore();
+  describe('#defineInitialState', function() {
+
+    it('has empty orders list', function() {
+      expect(vm.orders).to.be.empty;
+    });
+
+    it('uses current week', function() {
+      var begin = vm.week.startDate();
+      var end = vm.week.endDate().clone().add(1, 'day'); // because moment.isBetween() is exclusive
+
+      expect(moment().isBetween(begin, end)).to.be.true;
+    });
   });
 
-  it('has empty orders list as initial state', function() {
-    expect(vm.orders).to.be.empty;
-  });
+  describe('#fetchOrders', function() {
+    var client;
+    beforeEach(function() {
+      client = new Client({
+        firstName: 'Vasya',
+        lastName: 'Pupkin'
+      });
+    });
 
-  it('fetches the orders', function() {
-    vm.fetchOrders()
-      .then(function() {
-        expect(vm.orders).to.deep.equal([1,2,3]);
+    var order;
+    beforeEach(function() {
+      order = new Order(client);
+    });
+
+    beforeEach(function() {
+      sinon.stub(Order, 'findWhere').returns($q(function(resolve) {
+        resolve([order]);
+      }));
+    });
+
+    afterEach(function() {
+      Order.findWhere.restore();
+    });
+
+    it('returns an array of order instances', function() {
+      vm.fetchOrders()
+        .then(function() {
+          expect(vm.orders).to.be.an('array').that.have.property(0).that.is.an.instanceOf(Order);
+        });
+
+      resolvePromises();
+    });
+
+    context('when error occured', function() {
+
+      beforeEach(function() {
+        Order.findWhere.restore();
+        sinon.stub(Order, 'findWhere').returns($q(function(resolve, reject) {
+          reject();
+        }));
       });
 
-    resolvePromises();
-  });
+      it('logs error message if any error occured while fetching orders', function() {
+        vm.fetchOrders()
+          .then(function() {
+            expect($log.error).to.have.been.called;
+          });
 
-  it('uses current week when fetching orders', function() {
-    var begin = vm.week.startDate();
-    var end = vm.week.endDate().clone().add(1, 'day'); // because moment.isBetween() is exclusive
-
-    expect(moment().isBetween(begin, end)).to.be.true;
-  });
-
-  it('logs error message if any error occured while fetching orders', function() {
-    Order.findWhere.restore();
-    sinon.stub(Order, 'findWhere').returns($q(function(resolve, reject) {
-      reject();
-    }));
-
-    vm.fetchOrders()
-      .then(function() {
-        expect($log.error).to.have.been.called;
+        resolvePromises();
       });
-
-    resolvePromises();
+    });
   });
-
 });
