25b28b562f66131984f008318706e4e5703d57c4
dezoxel <dezoxel@gmail.com>
2015-08-13 20:54:36 +0300

    [Orders data] Added much more random client orders

--- src/app/entities/backend/orders_data.js
+++ src/app/entities/backend/orders_data.js
@@ -3,67 +3,89 @@
 
   angular
     .module('sfba.entities')
-    .constant('ordersData', {
-      office1: {
-        office: {
-          id: 1,
-          title: 'Office 1',
-          company: {
-            id: 1,
-            title: 'Cogniance'
-          }
-        },
-        week: {
-          startDate: '2015-01-05'
-        },
-        list: [{
-          client: {
-            firstName: 'Vasya',
-            lastName: 'Pupkin'
-          },
-          dishSet: {
-            mon: 'big_no_salad',
-            tue: 'mid_no_garnish',
-            thu: 'mid'
-          }
-        }, {
-          client: {
-            firstName: 'Ivan',
-            lastName: 'Ivanov'
-          },
-          dishSet: {
-            mon: 'big',
-            tue: 'big',
-            wed: 'big',
-            thu: 'big',
-            fri: 'big'
-          }
-        }]
-      },
-      office2: {
-        office: {
-          id: 2,
-          title: 'Office 2',
-          company: {
-            id: 1,
-            title: 'Cogniance'
-          }
-        },
-        week: {
-          startDate: '2015-01-05'
-        },
-        list: [{
-          client: {
-            firstName: 'Petro',
-            lastName: 'Petrovich'
-          },
-          dishSet: {
-            mon: 'mid_no_salad',
-            tue: 'mid_no_garnish',
-            fri: 'salad',
-            thu: 'mid'
-          }
-        }]
+    .factory('ordersData', function(dishSets) {
+
+      function randomInteger(min, max) {
+        var rand = min + Math.random() * (max + 1 - min);
+        rand = Math.floor(rand);
+        return rand;
       }
+
+      var names = {
+        office1: [
+          'Jack Larsen',
+          'Jerry Lewis',
+          'Robert Graves',
+          'Tom Vinson',
+          'Martin Gallagher',
+          'Brian Potter',
+          'Benjamin Snyder',
+          'Timothy Fletcher',
+          'Joshua Pratt',
+          'Connor Dalton',
+          'Robert Roberson',
+          'Pete Hebert',
+          'George McCarty',
+          'Jay Barry',
+          'Jessie Hayden',
+          'Nancy Griffin',
+          'Willie Oliver',
+          'Joyce Benson',
+          'Carol Houston',
+          'Joan Hill',
+          'Cassandra Pena',
+          'Brenda Prince',
+          'Daniela McBride',
+          'Brandi Macdonald'
+        ],
+        office2: [
+          'Theodore Dorsey',
+          'Paul Hines',
+          'Russell Vaughan',
+          'Tracy Neill',
+          'Hector Haley',
+          'Edward Cross',
+          'Georgia Conley',
+          'Lucy Rhodes',
+          'Kathy Calderon',
+          'Suzanne Christensen',
+          'Lois Swanson',
+          'Peggy Herring',
+          'Ebony Snider',
+          'Sabrina Walter',
+          'Ellie Lowe',
+          'Camila Kirk'
+        ]
+      };
+
+      var dishSetIDs = Object.keys(dishSets.list());
+      var weekdays = ['mon', 'tue', 'wed', 'thu', 'fri'];
+
+      function generateRandomOrdersFor(officeId) {
+        return names[officeId].map(function(name) {
+          var dishSet = {};
+          weekdays.forEach(function(weekday) {
+            dishSet[weekday] = dishSetIDs[randomInteger(0, dishSetIDs.length - 1)];
+          });
+
+          return {
+            client: {fullName: name},
+            dishSet: dishSet
+          };
+        });
+      }
+
+      return {
+        office1: {
+          office: {id: 1, title: 'Office 1', company: {id: 1, title: 'Cogniance'}},
+          week: {startDate: '2015-01-05'},
+          list: generateRandomOrdersFor('office1')
+        },
+        office2: {
+          office: {id: 2, title: 'Office 2', company: {id: 1, title: 'Cogniance'}},
+          week: {startDate: '2015-01-05'},
+          list: generateRandomOrdersFor('office2')
+        }
+      };
     });
 })(angular);
