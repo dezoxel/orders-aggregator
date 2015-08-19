7750466197350b283e6eed5693aeb2b6624a7315
dezoxel <dezoxel@gmail.com>
2015-08-19 12:37:25 +0300

    [Backend] Generators of names have been moved to the clientsData service

--- src/app/entities/backend/clients_data.js
+++ src/app/entities/backend/clients_data.js
@@ -5,17 +5,71 @@
     .module('sfba.entities')
     .factory('clientsData', function() {
 
+      var names = [
+        'Jack Larsen',
+        'Jerry Lewis',
+        'Robert Graves',
+        'Tom Vinson',
+        'Martin Gallagher',
+        'Brian Potter',
+        'Benjamin Snyder',
+        'Timothy Fletcher',
+        'Joshua Pratt',
+        'Connor Dalton',
+        'Robert Roberson',
+        'Pete Hebert',
+        'George McCarty',
+        'Jay Barry',
+        'Jessie Hayden',
+        'Nancy Griffin',
+        'Willie Oliver',
+        'Joyce Benson',
+        'Carol Houston',
+        'Joan Hill',
+        'Cassandra Pena',
+        'Brenda Prince',
+        'Daniela McBride',
+        'Brandi Macdonald',
+        'Theodore Dorsey',
+        'Paul Hines',
+        'Russell Vaughan',
+        'Tracy Neill',
+        'Hector Haley',
+        'Edward Cross',
+        'Georgia Conley',
+        'Lucy Rhodes',
+        'Kathy Calderon',
+        'Suzanne Christensen',
+        'Lois Swanson',
+        'Peggy Herring',
+        'Ebony Snider',
+        'Sabrina Walter',
+        'Ellie Lowe',
+        'Camila Kirk'
+      ];
+
       function randomInteger(min, max) {
         var rand = min + Math.random() * (max + 1 - min);
         rand = Math.floor(rand);
         return rand;
       }
 
+      var balances = [-250, -190, -100, -45, 0, 50, 100, 225];
+
       return {
         clientWithName: function(fullName) {
           return {
             id: randomInteger(1, 999),
-            fullName: fullName
+            fullName: fullName,
+            balance: randomInteger(0, balances.length - 1)
+          };
+        },
+
+        generateAny: function() {
+          return {
+            id: randomInteger(1, 999),
+            fullName: names[randomInteger(0, names.length - 1)],
+            balance: balances[randomInteger(0, balances.length - 1)]
           };
         }
       };
--- src/app/entities/backend/orders_data.js
+++ src/app/entities/backend/orders_data.js
@@ -3,7 +3,7 @@
 
   angular
     .module('sfba.entities')
-    .factory('ordersData', function(dishSets) {
+    .factory('ordersData', function(dishSets, clientsData) {
 
       function randomInteger(min, max) {
         var rand = min + Math.random() * (max + 1 - min);
@@ -11,70 +11,36 @@
         return rand;
       }
 
-      var names = {
-        office1: [
-          'Jack Larsen',
-          'Jerry Lewis',
-          'Robert Graves',
-          'Tom Vinson',
-          'Martin Gallagher',
-          'Brian Potter',
-          'Benjamin Snyder',
-          'Timothy Fletcher',
-          'Joshua Pratt',
-          'Connor Dalton',
-          'Robert Roberson',
-          'Pete Hebert',
-          'George McCarty',
-          'Jay Barry',
-          'Jessie Hayden',
-          'Nancy Griffin',
-          'Willie Oliver',
-          'Joyce Benson',
-          'Carol Houston',
-          'Joan Hill',
-          'Cassandra Pena',
-          'Brenda Prince',
-          'Daniela McBride',
-          'Brandi Macdonald'
-        ],
-        office2: [
-          'Theodore Dorsey',
-          'Paul Hines',
-          'Russell Vaughan',
-          'Tracy Neill',
-          'Hector Haley',
-          'Edward Cross',
-          'Georgia Conley',
-          'Lucy Rhodes',
-          'Kathy Calderon',
-          'Suzanne Christensen',
-          'Lois Swanson',
-          'Peggy Herring',
-          'Ebony Snider',
-          'Sabrina Walter',
-          'Ellie Lowe',
-          'Camila Kirk'
-        ]
-      };
-
       var dishSetIDs = Object.keys(dishSets.list());
       var weekdays = ['mon', 'tue', 'wed', 'thu', 'fri'];
 
-      function generateRandomOrdersFor(officeId) {
-        return names[officeId].map(function(name) {
-          var dishSet = {};
-          weekdays.forEach(function(weekday) {
-            dishSet[weekday] = dishSetIDs[randomInteger(0, dishSetIDs.length - 1)];
-          });
+      function generateRandomOrders() {
+        var orders = [];
+        for(var i = 0; i < 15; i++) {
 
-          return {
-            id: randomInteger(1, 999),
-            client: {fullName: name},
-            dishSet: dishSet,
+          orders.push({
+            id: generateId(),
+            client: clientsData.generateAny(),
+            dishSet: generateDishSet(),
             payments: generatePayments()
-          };
+          });
+        }
+
+        return orders;
+      }
+
+      function generateId() {
+        return randomInteger(1, 999);
+      }
+
+      function generateDishSet() {
+        var dishSet = {};
+
+        weekdays.forEach(function(weekday) {
+          dishSet[weekday] = dishSetIDs[randomInteger(0, dishSetIDs.length - 1)];
         });
+
+        return dishSet;
       }
 
       function generatePayments() {
@@ -96,12 +62,12 @@
         office1: {
           office: {id: 1, title: 'Office 1', company: {id: 1, title: 'Cogniance'}},
           week: {startDate: '2015-01-05'},
-          list: generateRandomOrdersFor('office1')
+          list: generateRandomOrders()
         },
         office2: {
           office: {id: 2, title: 'Office 2', company: {id: 1, title: 'Cogniance'}},
           week: {startDate: '2015-01-05'},
-          list: generateRandomOrdersFor('office2')
+          list: generateRandomOrders()
         }
       };
     });
